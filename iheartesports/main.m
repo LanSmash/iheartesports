fcf5070c73fa73e3269988270bfe2232d052acbb
Simon Pilot <me@webberist.com>
2015-05-07 21:51:59 +1000

    Initial Commit

---
+++ iheartesports/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iheartesports
+//
+//  Created by Simon Pilot on 7/05/2015.
+//  Copyright (c) 2015 LanSmash. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
